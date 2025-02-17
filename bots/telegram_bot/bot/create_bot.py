2bddff353853b5b907c862881908f49feafffcfe
Narek <narekbayanduryan16@gmail.com>
2025-02-17 16:18:50 +0300

    Fixed bug related to Dispatcher.

--- bots/telegram_bot/bot/create_bot.py
+++ bots/telegram_bot/bot/create_bot.py
@@ -1,8 +1,7 @@
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from telegram_bot.config import config
 
 
 bot = Bot(token=config.TELEGRAM_BOT_TOKEN)
-dp = Dispatcher(bot, storage=MemoryStorage())
+dp = Dispatcher()
