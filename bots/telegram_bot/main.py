898d8b194a578bd27014c775956f8c895ca752d6
Narek <narekbayanduryan16@gmail.com>
2025-02-17 16:17:57 +0300

    Filled out the 'telegram_bot.main' file and also created a function to indicate the correct import path.

--- bots/telegram_bot/main.py
+++ bots/telegram_bot/main.py
@@ -1,1 +1,27 @@
-# регистрация routers и запуск бота.
+import os
+import sys
+import asyncio
+from pathlib import Path
+
+def setup_paths():
+    """Устанавливаем правильные пути для импортов."""
+    current_dir = os.getcwd()
+    sys.path.append(current_dir)
+    
+    parent_dir = str(Path(current_dir).parent.absolute())
+    sys.path.append(parent_dir)
+
+setup_paths()
+
+from telegram_bot.bot.create_bot import bot, dp
+from telegram_bot.bot.handlers import register_all_routers
+
+register_all_routers(dp)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
